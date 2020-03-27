0c19ffd13a3d31104b545419e7064d794c860e56
Lisa Jung <lisahjung@gmail.com>
2020-03-27 21:01:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash= {}
   data.each do |property, hash|
-    hash.each do |attribute, array]
+    hash.each do |attribute, array|
       array.each do |name|
         
         if !new_hash.has_key?(name)
